b73566a155daa0d5a0d1fa860ff49167c3bd7ac6
Bryan <bryanstevenal@gmail.com>
2022-02-21 19:50:13 +0100

    [FRONT-END] Add aside component

--- fermon-frontend/src/app/app.module.ts
+++ fermon-frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AsideComponent } from './components/aside/aside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
