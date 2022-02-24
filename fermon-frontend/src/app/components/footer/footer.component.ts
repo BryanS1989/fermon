db4c2a2a98cbbe0c0893ca10a7dd83727d68f6c1
Bryan <bryanstevenal@gmail.com>
2022-02-24 19:30:30 +0100

    [FRONT-END] Footer first version

--- fermon-frontend/src/app/components/footer/footer.component.ts
+++ fermon-frontend/src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faLocationDot, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram, faTwitter, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +8,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  companyName = "Suministros Fermon S.L.";
+  fermonInfo = [
+    {
+      icon : faLocationDot,
+      href: "https://www.google.com/maps/place/Ferreter%C3%ADa+Fermon+(Suministros+Industriales)/@41.326641,2.0907591,19z/data=!3m1!4b1!4m5!3m4!1s0x12a49ec3fce03649:0xc97863cb29b928e!8m2!3d41.32664!4d2.0913063",
+      class : ['fa-bounce', 'fa-lg'],
+      show: false,
+      text : "Av. d'Apel·les Mestres, 9, 08820 Prat De Llobregat, Barcelona"
+    },
+    {
+      icon : faPhone,
+      href: 'tel:+34933790320',
+      class : ['fa-shake', 'fa-lg'],
+      show: false,
+      text : '(+34) 93 379 03 20'
+    },
+    {
+      icon : faEnvelope,
+      href: 'mailto:info@ferreteriafermon.com',
+      class : ['fa-beat', 'fa-lg'],
+      show: false,
+      text : 'info@ferreteriafermon.com'
+    }
+  ];
+  rrssInfo = [
+    {
+      icon: faLinkedin,
+      href: "https://www.linkedin.com"
+    },
+    {
+      icon: faInstagram,
+      href: "https://www.instagram.com"
+    },
+    {
+      icon: faTwitter,
+      href: "https://twitter.com/SumFermon"
+    },
+    {
+      icon: faFacebook,
+      href: "https://www.facebook.com/Ferreteria-Fermon-Suministros-Industriales-143460399448982"
+    }
+  ];
+  legalInfo = [
+    {
+      text : "Aviso legal",
+      href : ""
+    },
+    {
+      text : "Politica de privacidad",
+      href : ""
+    },
+    {
+      text : "Politica de cookies",
+      href : ""
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public toggleText(info : any) : void{
+    info.show = !info.show;
   }
 
 }
