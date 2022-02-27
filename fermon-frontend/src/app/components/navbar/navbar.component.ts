8a2bcd91340e95a71fd0fad145d0ff94dd43331f
Bryan <bryanstevenal@gmail.com>
2022-02-27 11:18:27 +0100

    [FRON-END] Create navbar component

--- fermon-frontend/src/app/components/navbar/navbar.component.ts
+++ fermon-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl } from '@angular/forms';
+
+import { faMagnifyingGlass, faUser, faCartShopping, faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  placeHolder = "¿Qué es lo que buscas?"
+
+  searchInput = new FormControl('');
+  searchValue = "";
+
+  showSideMenu = false;
+
+  searchIcon = faMagnifyingGlass;
+  userIcon = faUser;
+  cartIcon = faCartShopping;
+  menuIcon = faBars;
+  menuClose = faXmark;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.searchInput.valueChanges.subscribe(value => {
+      this.searchValue = value;
+    });
+  }
+
+  searchProducts() : void {
+
+  }
+
+  toggleSideMenu() : void {
+    this.showSideMenu = !this.showSideMenu;
   }
 
 }
