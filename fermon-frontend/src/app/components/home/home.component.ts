9be1edeee4cdfc5386d98f26676c81a17c72e9c1
Bryan <bryanstevenal@gmail.com>
2022-03-02 22:13:41 +0100

    [FRONT-END] add banner slider component to home component

--- fermon-frontend/src/app/components/home/home.component.ts
+++ fermon-frontend/src/app/components/home/home.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  banners = [
+    {
+      title: "FERRETERIA FERMON S.L.",
+      cite: "El servicio al cliente es nuestra máxima prioridad",
+      text: "Desde 1959 ofreciendo el mejor servicio a nuestros clientes, ofreciendo los mejores artículos al mejor precio",
+      button: {
+        text: "NOSOTROS",
+        route: "legal/policy"
+      },
+      image: "BANNER_IMAGE_1"
+    },
+    {
+      title: "Amplia experiencia en el sector",
+      cite: "Trabajamos con las primeras marcas",
+      text: "En Ferreteria FERMON S.L encontrarás las primeras marcas del sector",
+      button: {
+        text: "MARCAS",
+        route: "legal/policy"
+      },
+      image: "BANNER_IMAGE_2"
+    },
+    {
+      title: "Amplia experiencia en el sector",
+      cite: "Trabajamos con las primeras marcas",
+      text: "En Ferreteria FERMON S.L encontrarás las primeras marcas del sector",
+      button: {
+        text: "MARCAS",
+        route: "legal/policy"
+      },
+      image: "BANNER_IMAGE_3"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
