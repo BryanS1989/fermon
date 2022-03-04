b4eef3389b3d3c6d3bed3a1df70afc12b2b5f6ff
Bryan <bryanstevenal@gmail.com>
2022-03-04 19:34:47 +0100

    [FRONT-END] Create Aside component

--- fermon-frontend/src/app/components/aside/aside.component.ts
+++ fermon-frontend/src/app/components/aside/aside.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsideComponent implements OnInit {
 
+  menuClose = faXmark;
+
+  showSideMenu = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleSideMenu() : void {
+    this.showSideMenu = !this.showSideMenu;
+  }
 }
