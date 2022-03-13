52e5ad4554adcc0491ed1c99634725c2258abeb0
Bryan <bryanstevenal@gmail.com>
2022-03-13 18:15:48 +0100

    [FRONT-END] Create rate-product component

---
+++ fermon-frontend/src/app/components/rate-product/rate-product.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { faStar as fasStar, faStarHalfStroke, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
+
+@Component({
+  selector: 'app-rate-product',
+  templateUrl: './rate-product.component.html',
+  styleUrls: ['./rate-product.component.scss']
+})
+export class RateProductComponent implements OnInit {
+
+  @Input() rate : number = 0;
+  @Input() editable : boolean = false;
+
+  starsConfig : any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    var integerRate : number = Math.floor(this.rate);
+    var decimalRate : boolean = ( this.rate % 1 ) > 0;
+    var starRate : any[] = [];
+
+    console.log("integerRate: " + integerRate);
+    console.log("decimalRate: " + decimalRate);
+
+    for (let index = 0; index < 5; index++) {
+      var icon : IconDefinition = farStar;
+
+      if (index < integerRate) {
+        icon = fasStar ;
+      } else if ((index === integerRate) && decimalRate) {
+        icon = faStarHalfStroke;
+      }
+
+      starRate.push({
+        icon : icon
+      });
+    }
+
+    console.log(starRate);
+    this.starsConfig = starRate;
+  }
+
+}
