cdd23300fc78274ea6495c2125de80f961ee5a3a
Bryan <bryanstevenal@gmail.com>
2022-03-13 18:18:31 +0100

    [FRONT-END] Create Quantity-selector component

---
+++ fermon-frontend/src/app/components/quantity-selector/quantity-selector.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-quantity-selector',
+  templateUrl: './quantity-selector.component.html',
+  styleUrls: ['./quantity-selector.component.scss']
+})
+export class QuantitySelectorComponent implements OnInit {
+
+  @Output() quantitySelectedEvent :  EventEmitter<number> = new EventEmitter<number>();
+
+  quantitySelectorValues = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    15,
+    20
+  ];
+  quantitySelector = new FormControl(1);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  emitSelectedQuantity() : void {
+    this.quantitySelectedEvent.emit(this.quantitySelector.value);
+  }
+
+
+}
