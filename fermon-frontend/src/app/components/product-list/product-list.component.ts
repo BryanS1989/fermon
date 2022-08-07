05c7a62eb1e72042c5a4da648e77f95ed5b6ed5e
Bryan <bryanstevenal@gmail.com>
2022-08-07 11:41:34 +0200

    [FRONTEND] Modify produc prices from string to number

--- fermon-frontend/src/app/components/product-list/product-list.component.ts
+++ fermon-frontend/src/app/components/product-list/product-list.component.ts
@@ -15,7 +15,7 @@ export class ProductListComponent implements OnInit {
       id : 0,
       name : "Tornillo DIN 7985 Inox",
       shortDescription : "3mm de diametro",
-      price : "0.5",
+      price : 0.5,
       currency : "euro",
       category : [2],
       brandId : 0
@@ -24,7 +24,7 @@ export class ProductListComponent implements OnInit {
       id : 1,
       name : "Arandela DIN 125 Inox",
       shortDescription : "5mm de diametro",
-      price : "0.25",
+      price : 0.25,
       currency : "euro",
       category : [1, 0],
       brandId : 0
@@ -33,7 +33,7 @@ export class ProductListComponent implements OnInit {
       id : 2,
       name : "TUERCA DIN 985 INOX",
       shortDescription : "15mm de diametro",
-      price : "0.95",
+      price : 25.95,
       currency : "euro",
       category : [1, 0],
       brandId : 1
@@ -42,7 +42,7 @@ export class ProductListComponent implements OnInit {
       id : 3,
       name : "Varilla roscada DIN 975 Inox",
       shortDescription : "2cm de largo",
-      price : "1.25",
+      price : 1.25,
       currency : "euro",
       category : [1, 2],
       brandId : 2
@@ -51,7 +51,7 @@ export class ProductListComponent implements OnInit {
       id : 4,
       name : "Pistola 'ERGO GARDEN'",
       shortDescription : "Con tres modos de dispersión",
-      price : "19.95",
+      price : 19.95,
       currency : "euro",
       category : [3, 0, 0],
       brandId : 7
@@ -60,7 +60,7 @@ export class ProductListComponent implements OnInit {
       id : 5,
       name : "Tornillo DIN 7982 Inox",
       shortDescription : "1cm de diametro",
-      price : "0.1",
+      price : 0.1,
       currency : "euro",
       category : [2],
       brandId : 9
