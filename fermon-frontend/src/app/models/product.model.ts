566809423d2de3c0d3264526edd5f78166886d7a
Bryan <bryanstevenal@gmail.com>
2022-08-08 21:50:08 +0200

    [FRONTEND] Modify Product model

--- fermon-frontend/src/app/models/product.model.ts
+++ fermon-frontend/src/app/models/product.model.ts
@@ -1,27 +1,25 @@
 export class Product {
-  public id : number;
+  public id : number = -1;
+  public reference : string;
   public name : string;
   public shortDescription : string;
   public price : number;
   public currency : string;
   public category : number[];
   public brandId : number;
+  public image : string;
+  public rate : number = 0;
 
   constructor (
-    id : number,
-    name : string,
-    shortDescription : string,
-    price : number,
-    currency : string,
-    category : number[],
-    brandId : number
   ) {
-    this.id = id;
-    this.name = name;
-    this.shortDescription = shortDescription;
-    this.price = price;
-    this.currency = currency;
-    this.category = category;
-    this.brandId = brandId;
+    this.reference = "";
+    this.name = "";
+    this.shortDescription = "";
+    this.price = 0;
+    this.currency = "euro";
+    this.category = [0];
+    this.brandId = 0;
+    this.image = "";
+    this.rate = 0;
   }
 }
